7eaad876607953c04b9f2cab74b5171276e8388b
Kevin <862860000@qq.com>
2021-07-06 11:52:03 +0800

    modify docker images

--- scripts/build-dev-docker.sh
+++ scripts/build-dev-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build -f deployments/Dockerfile -t nilorg.azurecr.io/naas:dev .
-docker push nilorg.azurecr.io/naas:dev
+docker build -f deployments/Dockerfile -t nilorg/naas:dev .
+docker push nilorg/naas:dev
 
-# docker build -f deployments/Dockerfile.token-server -t nilorg.azurecr.io/naas-token-server:dev .
-# docker push nilorg.azurecr.io/naas-token-server:dev
+# docker build -f deployments/Dockerfile.token-server -t nilorg/naas-token-server:dev .
+# docker push nilorg/naas-token-server:dev
